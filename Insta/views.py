23d67c0f9ea25e9cb55f7342e916de17f9b121ba
Pochan9 <brian.pcchang@gmail.com>
2020-05-12 23:51:32 +0800

    Realize create-post and master/detail functions

--- Insta/views.py
+++ Insta/views.py
@@ -1,6 +1,20 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
-    
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
